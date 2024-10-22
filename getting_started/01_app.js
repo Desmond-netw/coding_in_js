e76433ef586543853f3dc01ae73fb4a2f5acbe58
Desmond_netw <dsquare200@gmail.com>
2024-10-21 23:57:26 -0700

    Getting js started

---
+++ getting_started/01_app.js
@@ -0,0 +1,26 @@
+//Task 1
+console.log("Aapo");
+console.log("Eero");
+console.log("Juhani");
+console.log("Lauri");
+console.log("Simeoni");
+console.log("Timo");
+console.log("Tuomas");
+
+//Task 2
+console.log("Row, row, row your boat,");
+console.log("Gently down the stream.");
+console.log("Merrily, merrily, merrily, merrily,");
+console.log("Life is but a dream.\n");
+
+//Task 3
+console.log("The number of minutes in a year:");
+console.log(365*24*60+ "\n"); 
+
+
+//Task 4
+//console.log('console.log("Hello there!")\n');
+
+const prompt = require("prompt-sync")();
+let name = prompt('Enter your name: ');
+console.log(`Hello, ${name}!`);
