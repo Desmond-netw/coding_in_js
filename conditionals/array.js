60744c20a7879b30b2f28c4e88284ef038f04098
Desmond-netw <dsquare200@gmail.com>
2024-10-28 00:35:22 +0200

    Example of arrya

---
+++ conditionals/array.js
@@ -0,0 +1,9 @@
+let rows = ["Naomi", "Bright", "Paul",];
+console.log(rows);
+
+// add push function
+let pushed = rows.push("John"); // print the length of rows
+console.log(pushed);
+console.log(rows);
+rows.pop();
+console.log(rows);
